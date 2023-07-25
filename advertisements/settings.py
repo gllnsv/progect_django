8211a1f65cf7d4ab5abfd3ddc0e29316e2e1341f
gllnsv <agalinka536@mail.ru>
2023-07-25 17:22:55 +0300

    added postgesql

--- advertisements/settings.py
+++ advertisements/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    '2': {
+        'ENGINE': 'django.db.backends.postgesql_psycog2',
+        'NAME': 'djangoproject',
     }
 }
 
